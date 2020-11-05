454d7db0facaac40a52dbf27bad72c0c02e4322d
RayChaung <a0919176442@gmail.com>
2020-11-05 10:55:03 +0800

    Store log at desktop

--- IoTServer.cpp
+++ IoTServer.cpp
@@ -20,7 +20,7 @@
 #include <map>
 #define PORT 7001 // Server's Port number
 #define MAXLENGTH 3000
-#define LogDIR "/home/pi/Desktop/IoT_Project/log/"
+#define LogDIR "/home/pi/Desktop/log/"
 
 using namespace std;
 using namespace boost::asio;
